5a010a517c68baeba6170c1a9272fc027ea22bc8
Tiago Costa <tiagodpv@gmail.com>
2023-03-07 21:26:44 -0500

    add color

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <Text>React Native</Text>
-      <Text>React Native</Text>
-    </>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24,
+      }}
+    >
+      <Text
+        style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48,
+        }}
+      >
+        Nome do Evento{' '}
+      </Text>
+
+      <Text
+        style={{
+          color: '#6B6B6B',
+          fontSize: 16,
+        }}
+      >
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+    </View>
   );
 }
