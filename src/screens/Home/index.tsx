d9a7d1a1fb3227921deaf23fb89ed8a377a5cd52
Tiago Costa <tiagodpv@gmail.com>
2023-03-12 16:51:12 -0400

    add Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log('voce clicou no botao de adicionar');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento </Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6B6B6B'}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
