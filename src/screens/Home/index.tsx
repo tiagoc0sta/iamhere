6c7d5e675a50aceb965118640b3b7e44cdd8205d
Tiago Costa <tiagodpv@gmail.com>
2023-03-12 19:58:09 -0400

    add participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,32 @@
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import { Participant } from '../../components/Participant';
 import { styles } from './styles';
 
 export function Home() {
+  const participants = [
+    'Tiago',
+    'Chris',
+    'Isabela',
+    'Miguel',
+    'Raí',
+    'Jhon',
+    'Paul',
+    'Jack',
+    'Jorge',
+  ];
+
   function handleParticipantAdd() {
     console.log('voce clicou no botao de adicionar');
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`voce clicou em remover o participante ${name}`);
   }
 
   return (
@@ -23,6 +46,24 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item} //make sure there is no repeated names
+            name={item}
+            onRemove={() => handleParticipantRemove('Tiago')}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes à sua lista
+          </Text>
+        )}
+      />
     </View>
   );
 }
