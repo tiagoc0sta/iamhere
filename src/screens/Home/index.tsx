fb6adbb9ba10d66030877ad6f945491b85d65a69
Tiago Costa <tiagodpv@gmail.com>
2023-03-18 17:13:31 -0400

    hoook2

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,26 +11,29 @@ import { Participant } from '../../components/Participant';
 import { styles } from './styles';
 
 export function Home() {
-  const [participants, setParticipants] = useState(['João']);
+  const [participants, setParticipants] = useState<string[]>(['']);
+
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
-    if (participants.includes('Tiago')) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         'Participante existe',
         'Já existe um participante na lista com esse nome'
       );
     }
 
-    setParticipants((prevstate) => [...prevstate, 'Ana']);
+    setParticipants((prevstate) => [...prevstate, participantName]);
+    setParticipantName('');
 
     //participants.push('Ana');
-    console.log(participants);
+    //console.log(participants);
 
     //console.log('voce clicou no botao de adicionar');
   }
 
   function handleParticipantRemove(name: string) {
-    Alert.alert('Remover', `Remover o participante ${name} ?`, [
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
       {
         text: 'Sim',
         onPress: () => Alert.alert('Deletado!'),
@@ -54,7 +57,9 @@ export function Home() {
         <TextInput
           style={styles.input}
           placeholder='Nome do participante'
-          placeholderTextColor={'#6B6B6B'}
+          placeholderTextColor='#6B6B6B'
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
